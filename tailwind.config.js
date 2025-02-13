fec71ad7f789e1fa7f46d849cdfa8b9d9ffc0ad6
JosueFG <josueflores13199@gmail.com>
2025-02-13 17:45:27 -0600

    Se agrego tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",  // Incluir archivos .html y .ts dentro de la carpeta src
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
